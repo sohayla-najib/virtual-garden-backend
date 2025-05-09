16be9a780d3cf5759ab47e9f9af87faeb09d75bf
sohayla-najib <najibsoula5@gmail.com>
2025-05-10 01:22:33 +0300

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,7 +1,7 @@
 // routes/home.routes.js
 const express = require('express');
 const router = express.Router();
-const HomeController = require('../controllers/homecontroller');
+const HomeController = require('../controllers/homeController');
 
 router.get('/', HomeController.welcome);
 router.get('/about', HomeController.aboutInfo);
